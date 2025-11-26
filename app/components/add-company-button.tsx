02d2cf274603c72f0d3d713178d2d9503479a1fc
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-11-26 21:20:01 +0400

    add libs/components/update content

---
+++ app/components/add-company-button.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { Button } from '@headlessui/react';
+import dynamic from 'next/dynamic';
+import React, { useState } from 'react';
+
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
+  );
+}
