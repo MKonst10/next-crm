f4b4bf8067b617bc9ca929206e12915d61f32c55
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-11-28 20:41:42 +0400

    add error handling

--- app/(admin)/companies/[id]/page.tsx
+++ app/(admin)/companies/[id]/page.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+
 import Header from '@/app/components/header';
+import { notFound } from 'next/navigation';
 
 export interface PageProps {
   params: Promise<{ id: string }>;
 }
 
 export default async function Page({ params }: PageProps) {
-  const { id } = await params;
+  const { id: rawId } = await params;
+  const companyId = Number(rawId);
+  if (Number.isNaN(companyId)) {
+    notFound();
+  }
   return (
     <>
-      <Header>Companies ({id})</Header>
+      <Header>Companies ({companyId})</Header>
     </>
   );
 }
